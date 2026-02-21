943a6b37c8d7dda778ca944e19e48a7162630f67
RachelBlack003 <rblack003@drury.edu>
2026-02-21 14:50:41 -0600

    Added smargandine (and extra custimization for the buttons)

--- reverse_turing/button.py
+++ reverse_turing/button.py
@@ -3,16 +3,18 @@ from pygame.surface import Surface
 from color import Color
 
 class Button:
-    def __init__(self, screen, text, x, y, size, padding=10):
+    def __init__(self, screen, text, x, y, size, padding=10, font_color = (0,0,0), bg_color = (240,240,240)):
         self.screen = screen
         self.text = text
         self.x = x
         self.y = y
         self.size = size
         self.padding = padding
+        self.color = font_color
+        self.bg_color = bg_color
 
         self.font = pygame.font.Font(pygame.font.get_default_font(), size)
-        self.font_surface = self.font.render(text, True, (0, 0, 0))
+        self.font_surface = self.font.render(text, True, font_color)
 
         self.text_rect = self.font_surface.get_rect(center=(x, y))
         self.button_rect = self.text_rect.inflate(padding * 2, padding * 2)
@@ -22,6 +24,6 @@ class Button:
 
     def render(self):
         mouse_pos = pygame.mouse.get_pos()
-        color = (200, 200, 200) if self.button_rect.collidepoint(mouse_pos) else (223, 223, 223)
+        color = tuple(int(col/1.15) for col in self.bg_color) if self.button_rect.collidepoint(mouse_pos) else self.bg_color
         pygame.draw.rect(self.screen, color, self.button_rect)
         self.screen.blit(self.font_surface, self.text_rect)
--- reverse_turing/questions/preference.py
+++ reverse_turing/questions/preference.py
@@ -1,4 +1,5 @@
 import pygame
+from button import Button
 
 class Preference:
     def __init__(self, controller):
@@ -6,6 +7,48 @@ class Preference:
         self.screen = controller.screen
         self.done = False
         self.font = pygame.font.SysFont(None, 40)
+
+        self.Burnt_Sienna = Button(
+            screen = self.screen,
+            text = "Burnt Sienna", 
+            x= 200,
+            y = 200, 
+            size = 40,
+            padding=15,
+            font_color= (255,255,255),
+            bg_color= (233, 116, 81)
+        )
+
+        self.Smaragdine = Button(
+            screen = self.screen,
+            text = "  Smaragdine  ", 
+            x= 600,
+            y = 200, 
+            size = 40,
+            padding=15,
+            font_color= (255,255,255),
+            bg_color= (74, 153, 118)
+        )
+        self.Banan_appeal = Button(
+            screen = self.screen,
+            text = "Banan-appeal", 
+            x= 600,
+            y = 400, 
+            size = 40,
+            padding=15,
+            font_color= (255,255,255),
+            bg_color= (252,235,183)
+        )
+        self.Blue = Button(
+            screen = self.screen,
+            text = "       Blue        ", 
+            x= 200,
+            y = 400, 
+            size = 40,
+            padding=15,
+            font_color= (255,255,255),
+            bg_color= (255,255,255)
+        )
 
     def handle_event(self, event):
         if event.type == pygame.KEYDOWN:
@@ -19,6 +62,10 @@ class Preference:
     def draw(self, screen):
         text = self.font.render("Preference", True, (255,255,255))
         screen.blit(text, (250, 300))
+        self.Smaragdine.render()
+        self.Burnt_Sienna.render()
+        self.Banan_appeal.render()
+        self.Blue.render()
 
     def is_done(self):
         return self.done
